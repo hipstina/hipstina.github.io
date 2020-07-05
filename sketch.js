bdc673488d58e3b98bcde23ea12c97a27e7f32fe
Christina P <17805011+hipstina@users.noreply.github.com>
2020-07-05 17:40:19 -0500

    added p5.js file, renamed script.js file

--- sketch.js
+++ sketch.js
@@ -1,43 +1,58 @@
-'use strict';
+// Sketch forked from /narrowdesign/sketches/s54BC2Lk
+// Rules:
+// All dot start at the center of their cell and point to the mouse cursor
+// The closer to the cursor a cell is, the closer the grid dot moves toward cursor
 
-window.addEventListener('scroll', underlineOnScroll);
-
-function underlineOnScroll() {
-    let sectionPosition0 = document.getElementById("welcome-section").getBoundingClientRect().bottom;
-    let sectionPosition1 = document.getElementById("about-me").getBoundingClientRect().bottom;
-    let sectionPosition3 = document.getElementById("conclusion").getBoundingClientRect().bottom;
-
-    if (window.scrollY > 0 && window.scrollY < sectionPosition0) {
-        document.getElementById("nav-item-A").classList.add('underline-dotted');
-        document.getElementById("nav-item-B").classList.add('underline-dotted');
-        document.getElementById("nav-item-C").classList.add('underline-dotted');
-    } else if (window.scrollY < sectionPosition1 && window.scrollY > sectionPosition0) {
-        document.getElementById("nav-item-A").classList.remove('underline-dotted');
-        document.getElementById("nav-item-A").classList.add('underline-solid');
-        document.getElementById("nav-item-B").classList.add('underline-dotted');
-        document.getElementById("nav-item-C").classList.add('underline-dotted');
-    } else if (window.scrollY > sectionPosition1 && window.scrollY < sectionPosition3 + document.documentElement.clientHeight) {
-        document.getElementById("nav-item-A").classList.add('underline-dotted');
-        document.getElementById("nav-item-B").classList.remove('underline-dotted');
-        document.getElementById("nav-item-B").classList.add('underline-solid');
-        document.getElementById("nav-item-C").classList.add('underline-dotted');
-    } else if (window.scrollY > sectionPosition3) {
-        document.getElementById("nav-item-A").classList.add('underline-dotted');
-        document.getElementById("nav-item-B").classList.add('underline-dotted');
-        document.getElementById("nav-item-C").classList.remove('underline-dotted');
-        document.getElementById("nav-item-C").classList.add('underline-solid');
-    } else {
-        document.getElementsByClassName("navItems").classList.add('underline-dotted');
-     
+function setup() {
+    size = Math.min(window.innerWidth, window.innerHeight);
+    createCanvas(window.innerWidth, window.innerHeight);
+    numCols = 28; // how many columns?
+    numRows = 23; // how many rows?
+    colorBase = 30;
+    radius = 10;
+  
+    cellW = canvas.width / 2 / numCols; // width of each cell
+    cellH = canvas.height / 2 / numRows; // height of each cell
+    lineW = cellW / 2;
+    lineH = cellH / 2;
+  }
+  
+  function draw() {
+    for (let col = 0; col < numCols; col++) {
+      // do this once for every column
+      for (let row = 0; row < numRows; row++) {
+        // do this once for every row
+        cellX = col * cellW;
+        cellY = row * cellH;
+        cellCenterX = cellX + lineW / 2;
+        cellCenterY = cellY + lineH / 2;
+        diffX = cellCenterX - mouseX;
+        diffY = cellCenterY - mouseY;
+        angle = atan2(diffY, diffX);
+        endX = cellCenterX - cos(angle) * lineW;
+        endY = cellCenterY - sin(angle) * lineH;
+  
+        // the line in the active cell is only as long as the distance to the cursor
+        if (Math.abs(diffX) < lineW && Math.abs(diffY) < lineH) {
+          endX = cellCenterX - diffX;
+          endY = cellCenterY - diffY;
+        }
+  
+        // 1) draw a box around the cell
+  
+        noStroke();
+        fill("#EFEFEF");
+        rect(cellX, cellY, cellW, cellH);
+  
+        // 2) draw lines from the center of the cell in the direction of the mouse
+        stroke("#0000B5");
+        strokeWeight(3);
+        line(endX, endY, endX, endY);
+      }
     }
-    
-} 
-
-    let navItems = Array.from(document.getElementsByClassName("underline-dotted"));
-    navItems.map(item => item.addEventListener("mouseover", underlineOnHover, false));
-    navItems.map(item => item.addEventListener("mouseout", removeUnderline, false));
-
-    function underlineOnHover() { event.currentTarget.setAttribute("style", "border-bottom:var(--red) solid 1.5px;") }
-    function removeUnderline() { event.currentTarget.setAttribute("style", "border-bottom:"); }
-    
-   
+    //cursor behavior
+    noStroke();
+    fill("rgb(237, 34, 93)");
+    circle(mouseX, mouseY, radius, radius);
+  }
+  
